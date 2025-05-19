674366291ec3f87e2446480e22c05c3c88d46ce7
sls_00 <1804048122@qq.com>
2025-05-19 22:23:49 +0800

    xx

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './button.css';
-
+// zxczxczzczx
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
   primary?: boolean;
